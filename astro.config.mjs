99c0a8ff7ffd8d2df7ef2c9aeed73544400af5ee
fonsito <ajhenriquez02@gmail.com>
2025-08-12 01:21:32 -0400

    feat: update dependencies and add Tailwind CSS support
    - Added @astrojs/react, @tailwindcss/vite, @types/react, @types/react-dom, lucide-react, react, and react-dom to package.json.
    - Updated Footer component to include a structured list and copyright notice.
    - Modified tsconfig.json to support React JSX syntax.
    - Created tarjeta.astro component file.
    - Added global.css file to import Tailwind CSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import icon from 'astro-icon';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+  integrations: [icon(), react()],
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
